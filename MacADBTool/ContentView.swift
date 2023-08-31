9675465da1ded2e073529c432ca8d555f12a438b
Wensly <Lucky_Nigel@yeah.net>
2023-09-01 00:49:11 +0800

    Update ContentView.swift

--- MacADBTool/ContentView.swift
+++ MacADBTool/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var terminalOutput: String = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 0) {
+            // Terminal Output View
+            TextEditor(text: $terminalOutput)
+                .font(.system(size: 14))
+                .border(Color.white, width: 1)
+                .padding()
+
+            // Buttons
+            HStack {
+                Button("扫描") {
+                    executeTerminalCommand("adb devices")
+                }
+                .padding()
+
+                Button("投屏") {
+                    executeTerminalCommand("scrcpy")
+                }
+                .padding()
+
+                Button("退出") {
+                    executeTerminalCommand("exit")
+                }
+                .padding()
+            }
+            .frame(maxWidth: .infinity)
+            .background(Color.gray.opacity(0.2))
         }
         .padding()
+        .frame(minWidth: 400, minHeight: 300)
+    }
+
+    private func executeTerminalCommand(_ command: String) {
+        let task = Process()
+        task.launchPath = "/opt/homebrew/bin/adb"
+        task.arguments = ["-c", command]
+
+        let pipe = Pipe()
+        task.standardOutput = pipe
+        task.standardError = pipe
+
+        let fileHandle = pipe.fileHandleForReading
+        task.launch()
+
+        let data = fileHandle.readDataToEndOfFile()
+        if let output = String(data: data, encoding: .utf8) {
+            terminalOutput = output
+        }
+        task.waitUntilExit()
     }
 }
 
