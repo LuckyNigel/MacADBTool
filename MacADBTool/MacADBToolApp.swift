8802d601ae8ed1be4dfe3421fef0f252e5ede3f2
Wensly <Lucky_Nigel@yeah.net>
2023-09-01 00:18:18 +0800

    Initial Commit

---
+++ MacADBTool/MacADBToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacADBToolApp.swift
+//  MacADBTool
+//
+//  Created by Wensly on 2023/9/1.
+//
+
+import SwiftUI
+
+@main
+struct MacADBToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
